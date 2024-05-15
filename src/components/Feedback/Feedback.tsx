cc88a730836f1156fe9f901ff0d8e03dfcc21d48
NataKyrylenko <natalik10107919@gmail.com>
2024-05-15 21:43:21 +0200

    add icons

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -3,6 +3,10 @@ import Button from "components/Button/Button";
 import { FeedbackContainer, FeedbackComponent, FeedbackCounter } from "./styles";
 import { feedbackSliceActions, feedbackSliceSelectors } from "store/redux/feedback/feedbackSlice";
 import { useAppDispatch, useAppSelector } from "store/hooks";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsDown, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { pointer } from "@testing-library/user-event/dist/cjs/pointer/index.js";
+
 
 function Feedback() {
 
@@ -20,20 +24,22 @@ function Feedback() {
  const resetResult = () => {
     dispatch(feedbackSliceActions.resetAll())
   }
-  
+
   return (
     <FeedbackContainer>
       <FeedbackComponent>
-        <Button name="Like" onButtonClick={onLike} />
+        <FontAwesomeIcon icon={faThumbsUp} size="4x" style={{ color: 'blue', cursor: 'pointer' }} onClick={onLike} />
         <FeedbackCounter>{countLike}</FeedbackCounter>
       </FeedbackComponent>
       <FeedbackComponent>
-        <Button name="Dislike" onButtonClick={onDislike} />
+      <FontAwesomeIcon icon={faThumbsDown} size="4x" style={{ color: 'blue', cursor: 'pointer' }} onClick={onDislike}/>
         <FeedbackCounter>{countDislike}</FeedbackCounter>       
       </FeedbackComponent>
       <FeedbackComponent>
         <Button name="Reset Results" onButtonClick={resetResult} />
       </FeedbackComponent>
+      
+      
     </FeedbackContainer>
   );
 }
--- src/components/Feedback/styles.ts
+++ src/components/Feedback/styles.ts
@@ -14,11 +14,14 @@ export const FeedbackContainer = styled.div`
 export const FeedbackComponent = styled.div`
   ${commonFeedbackStyles};
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   background-color: rgb(230, 232, 248);
   padding: 20px;
 `;
 
 export const FeedbackCounter = styled.p`
-  font-size: 28px;
+  font-size: 38px;
   font-weight: bold;
 `;
+
