c5d9732a5ef04f8589ab57e9544288def7164a6b
NataKyrylenko <natalik10107919@gmail.com>
2024-05-16 20:56:31 +0200

    lesson18

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import Homework16 from "homeworks/Homework16/Homework16"
 import Homework17 from "homeworks/Homework17/Homework17"
 //import Lesson16 from "lessons/Lesson16"
 
+import Layout from "pages/UsersApp/Layout/Layout"
+import Home from "pages/UsersApp/Home/Home"
+import Users from "pages/UsersApp/Users/Users"
+
 const App = () => {
   return (
+    <BrowserRouter>
+      {/* Lesson18 - Layout и его вложенные рауты */}
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="*" element="Page Not Found" />
+        </Routes>
+      </Layout>
+    
+    
+    </BrowserRouter>
+
+
+
+
     //  <Lesson16/>
     //  <Homework16/>
-    <Homework17 />
+    // <Homework17 />
+
+
   )
 }
 
---
+++ src/store/redux/users/usersSlice.ts
@@ -0,0 +1,23 @@
+import  {createAppSlice} from 'store/createAppSlice';
+import { UserData, UsersSliceState } from './types';
+import { PayloadAction } from '@reduxjs/toolkit';
+
+const usersInitialState: UsersSliceState = {
+    users: []
+}
+
+export const  usersSlice = createAppSlice({
+    name: 'USERS',
+    initialState: usersInitialState,
+    reducers: create => ({
+        addUser: create.reducer((state:UsersSliceState, action: PayloadAction<UserData>) => {
+            state.users = [...state.users, action.payload]
+        })
+    }),
+    selectors: {
+        users: (state: UsersSliceState) => state.users
+      }
+})
+
+export const usersSliseActions = usersSlice.actions;
+export const usersSliseSelectors = usersSlice.selectors;
